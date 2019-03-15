95ece6f473f7dbe878f9f3ed6697b913db365e5b
guotingmu <469022980@qq.com>
2019-03-15 23:56:57 +0800

    client

---
+++ config-client/src/main/java/com/demo/config/configclient/controller/ConfigController.java
@@ -0,0 +1,17 @@
+package com.demo.config.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
